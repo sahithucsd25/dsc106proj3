94e37cce05ff184d9d96816d8463166d2c745fc9
Nic Colebank <niccolebank@gmail.com>
2024-05-13 15:45:23 -0700

    added legend and header

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,12 @@
   import { onMount } from 'svelte';
   import * as d3 from 'd3';
   import * as topojson from 'topojson-client';
+  import { Legend } from '../src/helpers';
 
   let svgElement;
+  let legend;
   let incomeData = new Map();
+  let domain = [];
 
   onMount(async () => {
     const us = await d3.json("/states-albers-10m.json");
@@ -12,17 +15,18 @@
     csvData.filter(d => d.Category === "Households").forEach(d => {
       incomeData.set(d.State, d.median_income);
     });
+    const asArray = [...incomeData].map(([name, value]) => (Number(value)));
+    domain = d3.extent(asArray);
     initializeChart(us);
+    createLegend();
   });
 
   function initializeChart(us) {
     const width = 975;
     const height = 610;
     const colorScale = d3.scaleLinear()
-      .domain([52000, 102000]) //d3.extent(Array.from(incomeData.values()))
+      .domain(domain) //d3.extent(Array.from(incomeData.values()))
       .range(["lightgreen", "darkgreen"]); // Updated color scale from light green to dark green
-    console.log(incomeData.values())
-    console.log(d3.min(incomeData.values()))
 
     const svg = d3.select(svgElement)
         .attr("viewBox", `0 0 ${width} ${height}`)
@@ -53,6 +57,22 @@
       .attr("stroke-linejoin", "round")
       .attr("d", path(topojson.mesh(us, us.objects.states, (a, b) => a !== b)));
   }
+
+  function createLegend() {
+    const legendContainer = d3.select('#legend-container');
+    const legend = Legend(d3.scaleLinear(domain, ['lightgreen', 'darkgreen']), { title: 'Median Income', ticks: 5, });
+      legendContainer
+        .append(() => legend);
+  }
+  
+  
 </script>
 
-<svelte:element this="svg" bind:this={svgElement} />
+<main>
+  <div class='main'>
+    <h1>How does median income vary across the US?</h1>
+    <div class='legend' id='legend-container'/>
+    <svelte:element this="svg" bind:this={svgElement} />
+  </div>
+</main>
+
